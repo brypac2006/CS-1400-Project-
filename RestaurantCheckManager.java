49bcf2356d3baf13c9623704d3b8e3d174eb69e1
brypac2006 <brypac0506@gmail.com>
2024-10-06 18:09:40 -0700

    while

--- RestaurantCheckManager.java
+++ RestaurantCheckManager.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class RestaurantCheckManager() {
     System.out.println("Testing");
+    Scanner sc = new Scanner(System.in);
+    
+    String stop = "";
+    double totalAmount;
+    double totalSale;
+    double tipAmount;
+    
+    while (!stop.equals("y")) {
+
+
+
+        stop = sc.next();
+        stop = stop.toLowerCase();
+    }
 }
 
 
